708975a5de1e93869d5afba7318db5daf6a939ba
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-04-10 10:47:32 +0900

    0410 요일별 습관 조회 xml 파일 수정

---
+++ src/com/justhabit/model/dto/HaeinHabitDayDTO.java
@@ -0,0 +1,15 @@
+package com.justhabit.model.dto;
+
+public class HaeinHabitDayDTO {
+	private int UserID;
+	private int habitID;
+	private String habitName;
+	private int habitDays;
+	
+	
+	
+	
+	
+	
+	
+}
--- src/com/justhabit/run/Run.java
+++ src/com/justhabit/run/Run.java
@@ -1,5 +1,9 @@
 package com.justhabit.run;
 
+import static com.justhabit.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+
 import com.justhabit.view.FirstFrame;
 import com.justhabit.view.MainView;
 
@@ -10,6 +14,11 @@ public class Run {
 	public static void main(String[] args) {
 //		new LoginFrame().initialize();
 //		new MainView();
+		
+		/* 연결됐는지 확인하는 메소드 */
+//		Connection con = getConnection();
+//		System.out.println(con);
+		
 		
 	}
 }
