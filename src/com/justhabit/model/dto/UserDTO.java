3f17a5d5f67967d9fe3d191a8dd49192293fb308
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-04-10 11:34:11 +0900

    2021-04-10 UserDTO created

---
+++ src/com/justhabit/model/dto/UserDTO.java
@@ -0,0 +1,78 @@
+package com.justhabit.model.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7038215532379222271L;
+	private int userId;
+	private String userName;
+	private String userPwd;
+	private String userEmail;
+	private int userPin;
+	
+	public UserDTO() {
+		super();
+	}
+
+	public UserDTO(int userId, String userName, String userPwd, String userEmail, int userPin) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userPwd = userPwd;
+		this.userEmail = userEmail;
+		this.userPin = userPin;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public int getUserPin() {
+		return userPin;
+	}
+
+	public void setUserPin(int userPin) {
+		this.userPin = userPin;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", userName=" + userName + ", userPwd=" + userPwd + ", userEmail="
+				+ userEmail + ", userPin=" + userPin + "]";
+	}
+	
+	
+	
+}
